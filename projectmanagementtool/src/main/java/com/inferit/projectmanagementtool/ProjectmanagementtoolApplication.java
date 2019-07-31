8a8cb84c9f6c7297a8310f4420d992c18b96c3ca
Sajib Arafat Siddiqui <sajibarafatsiddiqui@gmail.com>
2019-07-31 22:04:57 +0600

    first commit

---
+++ projectmanagementtool/src/main/java/com/inferit/projectmanagementtool/ProjectmanagementtoolApplication.java
@@ -0,0 +1,13 @@
+package com.inferit.projectmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagementtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectmanagementtoolApplication.class, args);
+    }
+
+}
---
+++ projectmanagementtool/src/test/java/com/inferit/projectmanagementtool/ProjectmanagementtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inferit.projectmanagementtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectmanagementtoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
